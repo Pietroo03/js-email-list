39a7b6115e68fcd89d3adea1b2d54738229bf43c
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-17 15:56:11 +0200

    add changes to for cicle

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
-
 const emailListEl = document.querySelector('ul')
-console.log(emailListEl);
 
 
 
 
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then(response => {
-        console.log(response);
-        console.log(response.data.response);
-        for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
+    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then(response => {
+            console.log(response);
             const emailEl = response.data.response
             console.log(emailEl);
             const listItemEl = document.createElement('li')
             listItemEl.innerHTML = emailEl
             emailListEl.appendChild(listItemEl)
-        }
+        })
 
         
-    })
+    }
