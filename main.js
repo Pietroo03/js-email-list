4e4b4855b70c8bea9e79fbf6983134fbf47fce27
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-17 16:08:22 +0200

    add button event on JS doc

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 const emailListEl = document.querySelector('ul')
+const buttonEl = document.querySelector('button')
 
+buttonEl.addEventListener('click', () => {
+    emailListEl.innerHTML = ''
+    for (let i = 0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(response => {
+                console.log(response);
+                const emailEl = response.data.response
+                console.log(emailEl);
+                const listItemEl = document.createElement('li')
+                listItemEl.classList.add('col-4')
+                listItemEl.innerHTML = emailEl
+                emailListEl.appendChild(listItemEl)
+            }) 
+        }
+})
 
-
-
-for (let i = 0; i < 10; i++) {
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(response => {
-            console.log(response);
-            const emailEl = response.data.response
-            console.log(emailEl);
-            const listItemEl = document.createElement('li')
-            listItemEl.classList.add('col-4')
-            listItemEl.innerHTML = emailEl
-            emailListEl.appendChild(listItemEl)
-        }) 
-    }
