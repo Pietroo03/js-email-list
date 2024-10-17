319882bb09e0ed866a7272ae5f03e3f6947efad9
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-17 15:43:11 +0200

    add for cicle
    add markup

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+
+const emailListEl = document.querySelector('ul')
+console.log(emailListEl);
+
+
+
+
 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
     .then(response => {
+        console.log(response);
         console.log(response.data.response);
+        for (let i = 0; i < 10; i++) {
+            const emailEl = response.data.response
+            console.log(emailEl);
+            const listItemEl = document.createElement('li')
+            listItemEl.innerHTML = emailEl
+            emailListEl.appendChild(listItemEl)
+        }
+
         
     })
